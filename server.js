d0fe361845edab30b56626558a1d50eeab071eee
Bingo_Yan <yanhuibin315@sina.com>
2019-01-01 02:15:00 +0800

    添加判断

--- server.js
+++ server.js
@@ -5,36 +5,91 @@ var jieba = require('nodejieba');//分词
  
 from_submit();
 
-var response=function(){
-    this.more
+var result=function(){
+    this.status = 0;
+    this.otherSymptoms;
+    this.otherExamination;
+
 }
 
-function analys(input) {
+function analys(status,input) {
+    anwser = new result();
+
+    console.log(input);
     jieba.load({
         userDict: __dirname + '/data/disease.utf8',
     });
-    var topN = 4;
-    jieba.insertWord("男默女泪");
+
     // console.log(jieba.cutSmall(input,3));
-    console.log(jieba.cut(input));
+    // console.log(jieba.cut(input));
     // console.log(jieba.extract(input,5));
+
+    console.log(input);
+    if(status==1){
+        if(input.indexOf("感冒")>0){
+            anwser.res = "可能病症：流感，风热感冒，禽流感，流感嗜血杆菌感染，风寒感冒";
+        }
+    }else{
+        if((input.indexOf("怕冷")>0) && (input.indexOf("头痛")>0)){
+            anwser.res = "流感";
+        }else if((input.indexOf("发烧")>0) && (input.indexOf("咽痛")>0) && (input.indexOf("头痛")>0)){
+            anwser.res = "风热感冒";
+        }else if((input.indexOf("头痛")>0) && (input.indexOf("肚子痛")>0)){
+            anwser.res = "禽流感";
+        }else if((input.indexOf("发烧")>0) && (input.indexOf("咽痛")>0) && (input.indexOf("痉挛性咳嗽")>0)){
+            anwser.res = "流感嗜血杆菌感染";
+        }else if((input.indexOf("头痛")>0) && (input.indexOf("怕冷")>0)){
+            anwser.res = "风寒感冒";
+        }else{
+            anwser.res = "别担心，普通感冒，回去喝点板蓝根";
+        };
+    }
+    anwser.status = 1;
+    return anwser;
     
 }
 
 function from_submit() {
     var app = express();
-    var input1;
+
     var urlencodedParser = bodyParser.urlencoded({extended:false}); //创建编码解析
     app.use(express.static(path.join(__dirname, './'))); //添加本地文件到服务器中
     app.post('/analysis', urlencodedParser, function(req,res){
-        input1 = req.body.input1;
-        analys(input1);
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
-        res.send({
-            status:'1',
-            otherSymptoms:'<h3>其它症状</h3>说话带鼻音：<input type="radio" name="touteng" value="yes">是<input type="radio" name="touteng" value="no">否<br>持续咳嗽：<input type="radio" name="exercise" value="yes">是<input type="radio" name="exercise" value="no">否<br>发烧：<input type="radio" name="ate" value="yes">是<input type="radio" name="ate" value="no">否<br>斑丘疹：<input type="radio" name="zhen" value="yes">是<input type="radio" name="zhen" value="no">否<br>头痛：<input type="radio" name="tou" value="yes">是<input type="radio" name="tou" value="no">否<br>',
-            otherExamination:'<h3>相关检查</h3>耳鼻喉科：<input type="radio" name="inflammation" value="yes">鼻堵塞<input type="radio" name="inflammation" value="no">正常<input type="radio" name="inflammation" value="maybe">鼻发干<br>眼科：<input type="radio" name="temp" value="yes">发红<input type="radio" name="temp" value="no">含泪<input type="radio" name="temp" value="no">水肿<br><br><input class="coForm" type="submit" value="确定" >'
-        });
+        if(req.body.status=="1"){
+            console.log(req.body.status)
+            input0 = req.body.input0;
+            
+            ans = analys(1, input0);
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+            res.send({
+                res1:ans.res,
+                status:ans.status,
+                otherSymptoms:'<h3>其它症状</h3>体征表现：<input type="radio" name="input1" value="怕冷">怕冷<input type="radio" name="input1" value="正常">正常<br>头痛：<input type="radio" name="input2" value="头痛">是<input type="radio" name="input2" value="否">否<br>肚子痛：<input type="radio" name="input3" value="肚子痛">是<input type="radio" name="input3" value="否">否<br>流鼻涕：<input type="radio" name="input4" value="流鼻涕">是<input type="radio" name="input4" value="否">否<br>发烧：<input type="radio" name="input5" value="发烧">是<input type="radio" name="input5" value="否">否<br>',
+                otherExamination:'<h3>相关检查</h3>咽喉科：<input type="radio" name="input6" value="牙关紧闭">牙关紧闭<input type="radio" name="input6" value="正常">正常<input type="radio" name="input6" value="咽痛">咽红<br>眼科：<input type="radio" name="input7" value="正常">正常<input type="radio" name="input7" value="角弓反张">角弓反张<input type="radio" name="input7" value="眼睛水肿">水肿<br><br><input class="coForm" type="submit" value="确定" >'
+            });
+        }
+        else if(req.body.status=="2"){
+            input1 = req.body.input1;
+            input2 = req.body.input2;
+            input3 = req.body.input3;
+            input4 = req.body.input4;
+            input5 = req.body.input5;
+            input6 = req.body.input6;
+            input7 = req.body.input7;
+
+            input = input1+","+input2+","+input3+","+input4+","+input5+","+input6+","+input7;
+            inputAll = input0+":"+input;
+            ans2 = analys(req.body.status, input);
+            console.log(ans2);
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+            res.send({
+                inputAll:inputAll,
+                result:ans2.res
+
+            });
+        }
+        
+    
         
     })
 
